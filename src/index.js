6f0f0a9b35f761c15d443eaedb31b7435c22e821
bamboo_shoot <156806318+kon65138@users.noreply.github.com>
2025-01-31 19:20:46 +0000

    change hover logic for dropdown to simple css

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import './style.css';
 import threeDotsImg from './imgs/more_vert_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg';
 
-const topDropdownImg = document.querySelector('#topDropdown img');
-const bottomDropdownImg = document.querySelector('#bottomDropdown img');
+const topDropdownImg = document.querySelector('#topDropdownbtn img');
+const bottomDropdownImg = document.querySelector('#bottomDropdownbtn img');
 
 topDropdownImg.setAttribute('src', threeDotsImg);
 bottomDropdownImg.setAttribute('src', threeDotsImg);
 
-dropdown('topDropdown', 'topDropdownContent', 'hover');
-dropdown('bottomDropdown', 'bottomDropdownContent', 'click');
+dropdown('topDropdown', 'topDropdownbtn', 'topDropdownContent', 'hover');
+dropdown(
+  'bottomDropdown',
+  'bottomDropdownbtn',
+  'bottomDropdownContent',
+  'click',
+);
 
-function dropdown(btnId, dropdownContentId, clickOrHover) {
+function dropdown(dropdownContainerId, btnId, dropdownContentId, clickOrHover) {
   const dropdownContent = document.getElementById(dropdownContentId);
   const btn = document.getElementById(btnId);
   const dropdonwContentCompStyles = window.getComputedStyle(dropdownContent);
@@ -23,13 +28,6 @@ function dropdown(btnId, dropdownContentId, clickOrHover) {
       ) {
         dropdownContent.style = 'display: none;';
       }
-    });
-  } else if (clickOrHover.toLowerCase() === 'hover') {
-    btn.addEventListener('mouseover', () => {
-      dropdownContent.style = 'display: flex;';
-    });
-    dropdownContent.addEventListener('mouseleave', () => {
-      dropdownContent.style = 'display: none;';
     });
   }
 }
